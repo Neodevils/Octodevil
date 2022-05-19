836a90cd08fa6c16813b5f8fe53f09cf2b2b642c
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-05-19 10:47:28 +0300

    Add files via upload

---
+++ commands/chatbot.js
@@ -0,0 +1,15 @@
+module.exports = [
+	{
+		name: '$alwaysExecute',
+		code: `  
+		$reply[$messageID;no]
+		$jsonRequest[https://api.affiliateplus.xyz/api/chatbot?message=$message&botname=Aiden&ownername=Neo;message]
+		$botTyping
+
+		$onlyIf[$checkContains[$message;@everyone;@here]==false;{newEmbed:{description:I don't disturb people!}{color:ccb0b0}}]
+
+		$onlyForChannels[741841576992702505;{}]
+		$suppressErrors
+    `
+    }
+]
