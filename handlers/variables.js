7395f3bd45c748a3d931ab61bd97ba4f29872541
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-05-19 10:33:05 +0300

    Update variables.js

--- handlers/variables.js
+++ handlers/variables.js
@@ -1,14 +1,14 @@
 module.exports = (bot) => {
 //Embed Colors
   bot.variables({
-		brownHex: "#D7BBA7",
-		yellowHex: "#FFE8BA",
-		pinkHex: "#F8CDE2",
-		greenHex: "#BDFFB5",
-		redHex: "#FF7A7A",
-		orangeHex: "#FFAA7C",
-		purpleHex: "#DDB5FF",
-		blueHex: "#D4DEFF"
+    brownHex: "#D7BBA7",
+    yellowHex: "#FFE8BA",
+    pinkHex: "#F8CDE2",
+    greenHex: "#BDFFB5",
+    redHex: "#FF7A7A",
+    orangeHex: "#FFAA7C",
+    purpleHex: "#DDB5FF",
+    blueHex: "#D4DEFF"
   })
   
 //Staff Roles
@@ -21,107 +21,106 @@ module.exports = (bot) => {
   
 //Gender
   bot.variables({ 
-		him: "801610025558081567", 
-		her: "801609422438006794", 
-		them: "801610167606706186",
-		neo: "958991309031481364"
+    him: "801610025558081567", 
+    her: "801609422438006794", 
+    them: "801610167606706186",
+    neo: "958991309031481364"
   })
 
 //Age
   bot.variables({
-		13: "958997639683268619", 
-		15: "958997694897082418",
-		18: "958997737439911947",
-		21: "958997805983211561"
+    13: "958997639683268619", 
+    15: "958997694897082418",
+    18: "958997737439911947",
+    21: "958997805983211561"
   })
 
 //DM Status
   bot.variables({
-		open: "801611980971704341", 
-		closed: "801612315366785034",
-		ask: "959000233096937524"
+    open: "801611980971704341", 
+    closed: "801612315366785034",
+    ask: "959000233096937524"
   })
   
 //Color
-	bot.variables({
-		pyro: "801612652920438784",
-		geo: "801613602440216617",
-		anemo: "801615185096867871",
-		dendro: "805430953958309908",
-		cyro: "959044181190381609",
-		hydro: "805433136615653416",
-		electro: "805435812786470932"
-	})
+  bot.variables({
+    pyro: "801612652920438784",
+    geo: "801613602440216617",
+    anemo: "801615185096867871",
+    dendro: "805430953958309908",
+    cyro: "959044181190381609",
+    hydro: "805433136615653416",
+    electro: "805435812786470932"
+  })
   
 //Profile
   bot.variables({
-		shy: "959009334266830918",
-		selfConfident: "959010082140598274",
-		friendly: "959009466509049896",
-		cold: "959009541167648808",
-		nerd: "959010955407273994",
-		dumb: "959011041717678130"
+    shy: "959009334266830918",
+    selfConfident: "959010082140598274",
+    friendly: "959009466509049896",
+    cold: "959009541167648808",
+    nerd: "959010955407273994",
+    dumb: "959011041717678130"
   })
   
 //Notifications
   bot.variables({
-		reportPing: "959007092537192458",
-		codePing: "959007276402901032",
-		suggestionPing: "962292010310586429",
-		extras: "836338427578286111",
-		events: "801618365231398912",
-		wikiUpload: "940881871624089600",
-		announce: "801618406817136651",
-		welcome: "801618241046315080"
+    reportPing: "959007092537192458",
+    codePing: "959007276402901032",
+    suggestionPing: "962292010310586429",
+    extras: "836338427578286111",
+    events: "801618365231398912",
+    wikiUpload: "940881871624089600",
+    announce: "801618406817136651",
+    welcome: "801618241046315080"
   })
   
 //Cards
   bot.variables({
-		elite: "963676267218108417",
-		special: "928642084980793404",
-		starlight: "963765637027598357",
-		epic: "928641823033942077",
-		collector: "895307653851512914",
-		legend: "895308742642507817"
+    elite: "963676267218108417",
+    special: "928642084980793404",
+    starlight: "963765637027598357",
+    epic: "928641823033942077",
+    collector: "895307653851512914",
+    legend: "895308742642507817"
   })
   
 //Extra Personalities
-	bot.variables({
-		memer: "959034290392404019",
-		artist: "959034408445292585",
-		kawai: "959034564892835890",
-		petLover: "959034682748583946",
-		short: "959034866832392222",
-		tall: "959034955529347102",
-		gamer: "959035187176562718",
-		cool: "959035488310792252"
-	})
+  bot.variables({
+    memer: "959034290392404019",
+    artist: "959034408445292585",
+    kawai: "959034564892835890",
+    petLover: "959034682748583946",
+    short: "959034866832392222",
+    tall: "959034955529347102",
+    gamer: "959035187176562718",
+    cool: "959035488310792252"
+  })
 	
 //Economy
-	bot.variables({
-		profileTitle: "",
-		profileDescription: "",
-
-		money: "0"
-	})
+  bot.variables({
+    profileTitle: "",
+    profileDescription: "",
+    money: "0"
+  })
 
 //Others
-	bot.variables({
-        caseNum: "0",
-		keyRole: "805451556081500220",
-		msgWebhook: "8gDapddH2y832Oba8SYM_V9K2lpF5L9LMzPhj4a6ylOYUOvBugbgxnRFFwg91UQi8zb_",
-		supportMessage: "e",
-		endstamp:"0",
-		hoster:"",
-		prize:"",
-		joined:"0",
-		joinedusers:"",
-		ended:"false",
-		afks: "",
-		afk: "hayır",
-		afkss:"",
-		afkmm:"0",
-		afkc: "0",
-		afkaa:"No one has mentioned :3"
-	})
+  bot.variables({
+    caseNum: "0",
+    keyRole: "805451556081500220",
+    msgWebhook: "8gDapddH2y832Oba8SYM_V9K2lpF5L9LMzPhj4a6ylOYUOvBugbgxnRFFwg91UQi8zb_",
+    supportMessage: "e",
+    endstamp:"0",
+    hoster:"",
+    prize:"",
+    joined:"0",
+    joinedusers:"",
+    ended:"false",
+    afks: "",
+    afk: "hayır",
+    afkss:"",
+    afkmm:"0",
+    afkc: "0",
+    afkaa:"No one has mentioned :3"
+  })
 }
