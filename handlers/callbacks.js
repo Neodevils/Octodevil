15b72898ac3121ee111f5745ae5f28f0596303f8
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-05-19 10:27:42 +0300

    Update and rename callbacks.js to handlers/callbacks.js

---
+++ handlers/callbacks.js
@@ -0,0 +1,12 @@
+module.exports = (bot) => {
+  bot.onMessage();
+  bot.onMessageDelete();
+  bot.onMessageUpdate();
+  bot.onInteractionCreate();
+  bot.onJoin();
+  bot.onLeave();
+  bot.onChannelCreate();
+  bot.onChannelDelete();
+  bot.onChannelUpdate();
+  bot.onMemberUpdate();
+}
