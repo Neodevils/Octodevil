9cd0eda45b896333c1aa2be6bf3a989d79960ddb
neoaichan@gmail.com <neoaichan@gmail.com>
2022-05-30 11:29:29 +0300

    Gamble updated, status is now ph & tr timezone

--- commands/economy/gamble.js
+++ commands/economy/gamble.js
@@ -5,23 +5,23 @@ module.exports = {
   code: `
   $interactionReply[;$nonEscape[$if[$randomText[success;fail;fail;fail;success]==success;{newEmbed:
     {title:GAMBLING!}
-    {description:$customEmoji[kaeruNumbers] You've won **$$numberSeparator[$slashOption[amount]]**}
-    {field: **$random[0;100]%** win chance:>>> Rolled \`$random[6;10]\`﹒$username[$authorID]
+    {description:$customEmoji[kaeruNumbers] You've won $customEmoji[kaeruToken] **$numberSeparator[$slashOption[amount]] tokens!**}
+    {field: **$random[0;100]%** win chance, amazing!:>>> Rolled \`$random[6;10]\`﹒$username[$authorID]
 Rolled \`$random[1;5]\`﹒$username[$clientID]}
     {color:$getVar[pinkHex]}
-    {footer:+$$slashOption[amount]}
+    {footer:$slashOption[amount] tokens claimed}
   };{newEmbed:
     {title:uh, gambling..}
-    {description:$customEmoji[kaeruNumbers] You've lost **$$numberSeparator[$slashOption[amount]]**}
+    {description:$customEmoji[kaeruNumbers] You've lost $customEmoji[kaeruToken] **$numberSeparator[$slashOption[amount]] tokens...**}
     {field: **$random[0;100]%** win chance thou, but you lost..:>>> Rolled \`$random[1;5]\`﹒$username[$authorID]
 Rolled \`$random[6;10]\`﹒$username[$clientID]}
     {color:$getVar[pinkHex]}
-    {footer:-$$slashOption[amount]}
+    {footer:-$slashOption[amount] tokens lost}
   }]]]
 	
-  $if[$randomText[success;fail;fail;fail;success]==success;$setUserVar[money;$sum[$getUserVar[money];$slashOption[amount]]];$setUserVar[money;$sub[$getUserVar[money];$slashOption[amount]]]]
+  $setGlobalUserVar[token;$sum[$getGlobalUserVar[token];$if[$randomText[success;fail;fail;fail;success]==success;$slashOption[amount];-$slashOption[amount]]]]
 	
-  $onlyIf[$slashOption[amount]<=$getUserVar[money];{"content": "$nonEscape[$customEmoji[kaeruAlert]] You don't have enough money $nonEscape[:(]","ephemeral": "true", "options": {"interaction": true}}]
+  $onlyIf[$slashOption[amount]<=$getUserVar[token];{"content": "$nonEscape[$customEmoji[kaeruAlert]] You don't have enough __tokens__ $nonEscape[:(]","ephemeral": "true", "options": {"interaction": true}}]
 
   $onlyIf[$slashOption[amount]>0;{"content": "$nonEscape[ ehehe :) my smart cutie :*]","ephemeral": "true", "options": {"interaction": true}}]
   `
--- handlers/status.js
+++ handlers/status.js
@@ -1,8 +1,17 @@
 module.exports = (bot) => {
   bot.status({
-    text: "minesa",
-    type: "WATCHING", 
-    status: "dnd", 
+    text: "$formatDate[$dateStamp;LT] $timezone[Asia/Manila] PH",
+    type: 'STREAMING', 
+    status: 'dnd', 
+    url: 'https://discord.gg/C4hfT69K',
+    time: 6
+  });
+  
+  bot.status({
+    text: "$formatDate[$dateStamp;LT] $timezone[Turkey] TR",
+    type: 'STREAMING',
+    status: 'dnd',
+    url: 'https://discord.gg/C4hfT69K',
     time: 6
   })
 }
