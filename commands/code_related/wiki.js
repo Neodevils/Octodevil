5f2185aeb4748f0def5b6b84e8d1d1f7ca07a12f
neoaichan@gmail.com <neoaichan@gmail.com>
2022-06-17 19:46:44 +0300

    Updated wiki

--- commands/code_related/wiki.js
+++ commands/code_related/wiki.js
@@ -1,33 +1,40 @@
-module.exports = [{
-	name: "wiki",
-	type: 'interaction',
-	prototype: 'slash',
-	code: `
-	$interactionReply[$customEmoji[kaeruRepoUpload] A new wiki has been uploaded!;
-	{newEmbed:
-		{field:$customEmoji[kaeruVerified] File Name (Recommended):$slashOption[name].js}
-		{field:$customEmoji[kaeruNote] Note from Coder:>>> $slashOption[info]}
-		{color:$getVar[redHex]}
-		{image:$slashOption[image]}
-		{footer:This system made for the users who doesn't able to copy embed messages.}
-	};{actionRow:{button:Download:2:codeDownload:false:<:kaeruDownload:972527098168229908>}}]
+module.exports = [
+  {
+    name: "wiki",
+    type: 'interaction',
+    prototype: 'slash',
+    code: `
+	  $interactionReply[$customEmoji[mnsUploadRepo] __A new wiki has been uploaded!__
+	  ;{newEmbed:
+		  {field:$customEmoji[mnsVerified] File Name (Recommended):$slashOption[name].js}
+		  {field:$customEmoji[mnsNote] Note from Coder:>>> $slashOption[info]}
+		  {color:$getVar[blueHex]}
+		  {image:$slashOption[image]}
+		  {footer:This system made for the users who doesn't able to copy embed messages.}
+	  }
+	  ;{actionRow:
+	    {button:Download:2:codeDownload:false:<:mnsDownload:987360980474400808>}
+	  }
+	  ]
 	
-	$onlyForIDs[$botOwnerID;{
-		"content": "$nonEscape[$customEmoji[kaeruAlert]] This can be made only by devs for your sake, but you can suggest the code.", 
-		"ephemeral": true, 
-		"options": {
-			"interaction":true 
-		}
-	}]
-	`
-}, {
-	name: "codeDownload",
-	type: 'interaction',
-	prototype: 'button',
-	code: `
-	$interactionReply[> $customEmoji[kaeruVerified] Feel free to download the file!
-	;
-	;
-	;{file:$readFile[./templates/$getEmbed[$channelID;$interactionData[message.id];1;field1.value]]:$getEmbed[$channelID;$interactionData[message.id];1;field1.value]};;yes]
-	`
-}]
+	  $onlyForIDs[$botOwnerID;{
+		  "content": "$nonEscape[$customEmoji[mnsAlert]] This can be made only by Neo for your sake, but you can suggest the code.", 
+		  "ephemeral": true, 
+		  "options": {
+			  "interaction":true 
+	  	}
+  	}]
+	  `
+  }, {
+    name: "codeDownload",
+    type: 'interaction',
+    prototype: 'button',
+    code: `
+	  $interactionReply[> $customEmoji[mnsVerified] Feel free to download the file!
+	  ;
+	  ;
+	  ;{file:$readFile[./templates/$getEmbed[$channelID;$interactionData[message.id];1;field1.value]]:$getEmbed[$channelID;$interactionData[message.id];1;field1.value]}
+	  ;;yes]
+	  `
+  }
+]
