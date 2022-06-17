02bf8eb54ff1d1f05c380d4ba93df8fb19b68caa
neoaichan@gmail.com <neoaichan@gmail.com>
2022-06-17 19:57:49 +0300

    Updated commission.

--- commands/economy/commission.js
+++ commands/economy/commission.js
@@ -4,7 +4,7 @@ module.exports = [
     type: 'interaction',
     prototype: 'slash',
     code: `
-    $interactionModal[Commission Meeting;commissionModal;{actionRow:{textInput:Please type "$getGlobalUserVar[randomString]" on the field 👉👈:1:commissionInput:yes:Please write the meeting password on the above hehe:$charCount[$getGlobalUserVar[randomString]]:$charCount[$getGlobalUserVar[randomString]]}}]
+    $interactionModal[Commission Meeting;commissionModal;{actionRow:{textInput:Please type "$getGlobalUserVar[randomString]" on the field.:1:commissionInput:yes:Please write the meeting password on the above hehe:$charCount[$getGlobalUserVar[randomString]]:$charCount[$getGlobalUserVar[randomString]]}}]
   
     $setGlobalUserVar[randomString;$randomString[$random[4;12]]]
     `
@@ -17,7 +17,7 @@ module.exports = [
     $setGlobalUserVar[token;$sum[$getGlobalUserVar[token];$randomText[100;100;100;100;100;100;100;100;400;400;400;400;1200;1200;1200]]]
     
     $interactionReply[
-    ;$nonEscape[$if[$getGlobalUserVar[randomString]==$textInputValue[commissionInput];{newEmbed:{title:$customEmoji[kaeruVerified] \`$textInputValue[commissionInput]\`}{description:$customEmoji[kaeruToken] × $randomText[100;100;100;100;100;100;100;100;400;400;400;400;1200;1200;1200] AKI Tokens sent to your account. ヾ(〃^∇^)ﾉ}{color:$getVar[blueHex]}{image:https://media.discordapp.net/attachments/861208192121569280/981529632639684658/kaeru_n_octadex_commission.gif}{footer:Let's leave everything to Octadex 𖤪}};{newEmbed:{title: $customEmoji[kaeruAlert] \`$textInputValue[commissionInput]\` was wrong...}{description:㏒ — No tokens received in this commission. (╯•﹏•╰)}{color:$getVar[redHex]}{image:https://media.discordapp.net/attachments/861208192121569280/981558017369718804/kaeru_error.gif}{footer:We should not fail Octadex...}}]]
+    ;$nonEscape[$if[$getGlobalUserVar[randomString]==$textInputValue[commissionInput];{newEmbed:{title:$customEmoji[mnsVerified] \`$textInputValue[commissionInput]\`}{description:$customEmoji[mnsToken] × $randomText[100;100;100;100;100;100;100;100;400;400;400;400;1200;1200;1200] AKI Tokens sent to your account. ヾ(〃^∇^)ﾉ}{color:$getVar[invisibleHex]}{image:https://media.discordapp.net/attachments/861208192121569280/981529632639684658/kaeru_n_octadex_commission.gif}{footer:Let's leave everything to Octadex 𖤪}};{newEmbed:{title: $customEmoji[mnsImportant] \`$textInputValue[commissionInput]\` was wrong...}{description:㏒ — No tokens received in this commission. (╯•﹏•╰)}{color:$getVar[gitHex]}{image:https://media.discordapp.net/attachments/861208192121569280/981558017369718804/kaeru_error.gif}{footer:We should not fail Octadex...}}]]
     ;$nonEscape[$if[$getGlobalUserVar[randomString]==$textInputValue[commissionInput];{actionRow:{selectMenu:comm:𓊈・・・Mission Accomplished・・・𓊉:1:1:yes:{selectMenuOptions:-:-}}};{actionRow:{selectMenu:comm:𓊈・・・Mission Failed・・・𓊉:1:1:yes:{selectMenuOptions:-:-}}}]]]
     
     `
