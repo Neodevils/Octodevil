53a5a7615f145b4c63a5c752d6699b3e64afc72b
neoaichan@gmail.com <neoaichan@gmail.com>
2022-06-26 23:29:20 +0300

    Updated support.js

--- commands/others/support.js
+++ commands/others/support.js
@@ -3,11 +3,21 @@ module.exports = [
 	  name: "supMsg",
 		type: 'awaited',
 		code: `
-		$channelSendMessage[$channelID;{
-		  "content": "$nonEscape[$customEmoji[kaeruIssue]] **— How can we help you? :3**",
-		  "embeds": "{newEmbed:{thumbnail:$userAvatar[$clientID]}{description:>>> Please select a menu that you're requesting for help (≡^∇^≡)}{color:#fabd78}{footer:MinesaHub Help Desk:$serverIcon}{timestamp}{image:https://media.discordapp.net/attachments/774748031589023764/846327920905420810/Untitled1254_20201118215541.png}}",
-		  "components": "{actionRow:{selectMenu:supMenu_$getServerVar[caseNum]:Please select a thread type:1:1:no:{selectMenuOptions:Coding:sup0:— Stuck on a part of coding? Let us help to you.(๑'ᴗ')ゞ:no:<:kaeruDiscussCode:969585474140250172>}{selectMenuOptions:Reporting:sup1:— Did someone break the rules? We would like to know it and do the action! ʕ•ᴥ•ʔﾉ♡:no:<:kaeruDiscussReport:969347837123002469>}{selectMenuOptions:Others:sup2:— Suggesting, partnershipping or for wanting to claim rewards ヾ(´︶\`♡)ﾉ:no:<:kaeruDiscussOthers:969569146465579018>}}}"
-		}]
+		$apiMessage[$channelID
+		;$customEmoji[git_issue] — How can we help to you?
+		;{newEmbed:
+		  {thumbnail:https://media.discordapp.net/attachments/861208192121569280/990687631581016145/Balksz140_20220626213833.png}
+		  {description:> Please select a menu that you're requesting for help.}
+		  {color:$getVar[invisibleHex]}
+		  {footer:MinesaHub Help Desk:$serverIcon}
+		  {timestamp}
+		}
+		;{actionRow:{selectMenu:supMenu_$getServerVar[caseNum]:Select The Issue Type:1:1:no:
+		  {selectMenuOptions:Coding Issue:sup0:— Stuck on a part of coding? Let us help to you 👋:no:<:git_code_review:990691387886419989>}
+		  {selectMenuOptions:General Issue:sup1:— Have an issue with members, server or anything else?:no:<:git_review_requested:990691558716239913>}
+		  {selectMenuOptions:Others:sup2:— Suggesting, partnershipping or asking any questions to staff team.:no:<:git_comment:990691505847033896>}
+		}}
+		]
 	  `
 	},
   {
@@ -18,24 +28,23 @@ module.exports = [
     
 		$channelSendMessage[$get[id];Hello <@&959007276402901032> team, <@$authorID> needs your help :3]
 		
-		$let[id;$createThread[$channelID;✾ Coding Thread﹕Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
+		$let[id;$createThread[$channelID;⨀ New Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
 
 		$interactionUpdate[;{newEmbed:
 		  {title:$customEmoji[kaeruCodingLabel1]$customEmoji[kaeruCodingLabel2]}
 			{thumbnail:$authorAvatar}
-			{description:$customEmoji[kaeruIssue] Created Issue succesfully! 
-> *Since thread created for you, you can send your error and code to thread. We will check it as soon as possible!*}
-			{color:$getVar[purpleHex]}
-			{footer:MinesaHub Coding support:$serverIcon}
+			{description:$customEmoji[git_issue] Created Issue succesfully! 
+> *Since created thread for you, you can send your error and code to thread. We will check it as soon as possible.*}
+			{color:$getVar[gitHex]}
+			{footer:MinesaHub Coding Support:$serverIcon}
 			{timestamp}
 		};{actionRow:
-			{selectMenu:eee:e:1:1:yes:
-				{selectMenuOptions:code support thread has been created!:sup0:e:yes:<:kaeruDiscussCode:969585474140250172>}
+			{selectMenu:a::1:1:yes:
+				{selectMenuOptions:Code Support Thread Created!:sup0:e:yes:<:git_code_review:990691387886419989>}
 			}
 		}]
-		$setServerVar[caseNum;$sum[$getServerVar[caseNum];1]]
 		
-		$cooldown[1h;{"content" : "$nonEscape[$customEmoji[kaeruAlert]] You've already created an issue thread, but you can also wait %time% to create a new one :3","ephemeral" : true,"options" : {"interaction" : true}}]
+		$setServerVar[caseNum;$sum[$getServerVar[caseNum];1]]
 		
     $onlyIf[$interactionData[values[0]]==sup0;]
 		`
@@ -48,25 +57,23 @@ module.exports = [
 		
 		$channelSendMessage[$get[id];<@&959007092537192458>, <@$authorID> wants to report something!]
 		
-		$let[id;$createThread[$channelID;❖ Report Thread﹕Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
+		$let[id;$createThread[$channelID;⨀ New Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
 
 		$interactionUpdate[;{newEmbed:
 			{title:$customEmoji[kaeruReportLabel1]$customEmoji[kaeruReportLabel2]}
 			{thumbnail:$authorAvatar}
-			{description:$customEmoji[kaeruIssue] Created Issue succesfully!
-> *Please give details as much as you can, so report team can do more things!*}
-			{color:$getVar[redHex]}
-			{footer:MinesaHub Reporting:$serverIcon}
+			{description:$customEmoji[git_issue] Created Issue succesfully!
+> *Please give details as much as you can, so report team can understand you better!*}
+			{color:$getVar[gitHex]}
+			{footer:MinesaHub Staffs Team:$serverIcon}
 			{timestamp}
 		};{actionRow:
-			{selectMenu:ae:e:1:1:yes:
-				{selectMenuOptions:reporting thread has been created!:sup0:e:yes:<:kaeruDiscussReport:969347837123002469>}
+			{selectMenu:a::1:1:yes:
+				{selectMenuOptions:reporting thread has been created!:sup0:e:yes:<:git_review_requested:990691558716239913>}
 			}
 		}]
 
 		$setServerVar[caseNum;$sum[$getServerVar[caseNum];1]]
-		
-		$cooldown[1h;{"content" : "$nonEscape[$customEmoji[kaeruAlert]] You've already created an issue thread, but you can also wait %time% to create a new one :3","ephemeral" : true,"options" : {"interaction" : true}}]
 		
     $onlyIf[$interactionData[values[0]]==sup1;]
 		`
@@ -79,25 +86,23 @@ module.exports = [
 		
 		$channelSendMessage[$get[id];<@&962292010310586429>, <@$authorID> wants to discuss about something!]
 		
-		$let[id;$createThread[$channelID;☆ Others Thread﹕Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
+		$let[id;$createThread[$channelID;⨀ New Issue ⌗$getServerVar[caseNum];60;private;$messageID;yes]]
 
 		$interactionUpdate[;{newEmbed:
 			{title:$customEmoji[kaeruOthersLabel1]$customEmoji[kaeruOthersLabel2]}
 			{thumbnail:$authorAvatar}
-			{description:$customEmoji[kaeruIssue] Created Issue succesfully!
-> *We would like to hear out what you really want to talk about :3*}
-			{color:6b7783}
-			{footer:MinesaHub Others:$serverIcon}
+			{description:$customEmoji[git_issue] Created Issue succesfully!
+> *We would like to discuss about what you're talking about.*}
+			{color:$getVar[blueHex]}
+			{footer:MinesaHub Discussion:$serverIcon}
 			{timestamp}
 		};{actionRow:
-			{selectMenu:ae:e:1:1:yes:
-				{selectMenuOptions:others type thread has been created!:sup0:e:yes:<:kaeruDiscussOthers:969569146465579018>}
+			{selectMenu:a::1:1:yes:
+				{selectMenuOptions:others type thread has been created!:sup0:e:yes:<:git_comment:990691505847033896>}
 			}
 		}]
 
 		$setServerVar[caseNum;$sum[$getServerVar[caseNum];1]]
-		
-		$cooldown[1h;{"content" : "$nonEscape[$customEmoji[kaeruAlert]] You've already created an issue thread, but you can also wait %time% to create a new one :3","ephemeral" : true,"options" : {"interaction" : true}}]
     
     $onlyIf[$interactionData[values[0]]==sup2;]
 		`
