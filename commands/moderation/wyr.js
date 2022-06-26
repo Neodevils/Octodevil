094d5bf0b1e4c2e2610c8079a67abbb43e30b500
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-06-26 13:10:19 +0000

    Updated would you rather.

--- commands/moderation/wyr.js
+++ commands/moderation/wyr.js
@@ -3,7 +3,7 @@ module.exports = {
   type: 'interaction',
   prototype: 'slash',
   code: `
-  $sendMessage[{"embeds":"{newEmbed:{thumbnail:https://media.discordapp.net/attachments/861208192121569280/990524072196702248/09-32-28-protected_tags.png}{description:˚｡ *╭ ₊˚✿ would you rather ₊˚✿੭ ʕ · ﻌ · ʔ\\n$nonEscape[$customEmoji[mnsMilestone]] ┊ Question: **Would you rather...**\\n๑ ꒦︶︶꒷︶︶︶꒷꒦ ɞ\\n$nonEscape[$customEmoji[mnsTag]]  ┊。<:br_number_1:837050777129451550> $interactionData[options.data[0].value]\\n ┊or\\n$nonEscape[$customEmoji[mnsTag]]  ┊。<\:y_number_2\:837050845325426729> $interactionData[options.data[1].value]}{footer:$username asked:$authorAvatar}{timestamp}{color:$getVar[invisibleHex]}}", "fetchReply" : true,"options": {"reactions" : ["837050777129451550","837050845325426729"],"interaction" : true}}]
+  $sendMessage[{"embeds":"{newEmbed:{thumbnail:https://media.discordapp.net/attachments/861208192121569280/990524072196702248/09-32-28-protected_tags.png}{description:˚｡ *╭ ₊˚✿ would you rather ₊˚✿੭ ʕ · ﻌ · ʔ\\n$nonEscape[$customEmoji[mnsMilestone]] ┊ Question: **Would you rather...**\\n๑ ꒦︶︶꒷︶︶︶꒷꒦ ɞ\\n$customEmoji[mnsTag] ┊ $customEmoji[mnsPrevious] 。$slashOption[this]\\n ┊or\\n$customEmoji[mnsTag] ┊ $customEmoji[mnsNext] 。$slashOption[that]}{footer:$username asked:$authorAvatar}{timestamp}{color:$getVar[invisibleHex]}}", "fetchReply" : true,"options": {"reactions" : ["954552276717043812","954552202654019644"],"interaction" : true}}]
 
     $onlyForRoles[$getVar[owner];$getVar[admin];$getVar[mod];{"embeds":"{newEmbed:{description:$customEmoji[mnsImportant] You don't have a staff role to make would you rather~}{color:$getVar[invisibleHex]}}","ephemeral": "true", "options": {"interaction": true}}]
   `
