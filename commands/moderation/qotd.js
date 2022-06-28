c7f04729500658de0d0f0e5812946135e86cf4e2
neoaichan@gmail.com <neoaichan@gmail.com>
2022-06-28 10:56:34 +0300

    Updated qotd.

--- commands/moderation/qotd.js
+++ commands/moderation/qotd.js
@@ -3,12 +3,12 @@ module.exports = {
   type: 'interaction',
   prototype: 'slash',
   code: `
-  $channelSendMessage[$get[id2];<a:dp_star6:835142297125519420> <@&$getServerVar[extras]> hiii! lemme hear your thoughts about it *!*]
+  $channelSendMessage[$get[id2];<a:dp_star6:835142297125519420> <@&$getServerVar[extras]> Hii, let us hear your opinion 🤠]
   $wait[1s]
-  $let[id2;$createThread[$channelID;・𓄼 $replaceText[$formatDate[$dateStamp;L];/; ⁄ ]'s aotd ﹆;1440;public;$get[id];yes]]
+  $let[id2;$createThread[$channelID;➜ $replaceText[$formatDate[$dateStamp;L];/; ⁄ ]'s AOTD ﹆;1440;public;$get[id];yes]]
 
-  $let[id;$sendMessage[{"content":"$nonEscape[$customEmoji[kaeruDiscuss]] *$username asks question of the day!*", "embeds":"{newEmbed:{thumbnail:$serverIcon}{description:*No off-topic in thread pwease, talk about what is the topic about :3*\\n✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・\\n> **__question__: $interactionData[options.data[0].value]** \\n ✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・}{color:$getVar[pinkHex]}}", "fetchReply": true,"options":{"interaction":true, "reactions": ["958968024868737084"]}};yes]]
+  $let[id;$sendMessage[{"content":"$nonEscape[$customEmoji[git_discussion]] *$username asks question of the day!*", "embeds":"{newEmbed:{thumbnail:$serverIcon}{description:*No off-topic in thread please, talk about what is the topic about \`❤️\`*\\n✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・\\n> **__question__: $interactionData[options.data[0].value]** \\n ✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・}{color:$getVar[gitHex]}}", "fetchReply": true,"options":{"interaction":true, "reactions": ["991248874238910524"]}};yes]]
 
-  $onlyForRoles[$getVar[owner];$getVar[admin];$getVar[mod];{"content":"$customEmoji[kaeruAlert] You don't have any staff role to make question of the daily (｡ﾉ_ﾉ)","ephemeral": "true", "options": {"interaction": true}}]
+  $onlyForRoles[$getVar[owner];$getVar[admin];$getVar[mod];{"content":"$customEmoji[git_alert] You don't have any staff role to make question of the daily.","ephemeral": "true", "options": {"interaction": true}}]
   `
 }
