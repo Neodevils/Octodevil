5f00c937fec75070c8d4c7f3b7c75951e97e0681
neoaichan@gmail.com <neoaichan@gmail.com>
2022-06-28 11:10:39 +0300

    Updated qotd, $nonEscape was missing.

--- commands/moderation/qotd.js
+++ commands/moderation/qotd.js
@@ -9,6 +9,6 @@ module.exports = {
 
   $let[id;$sendMessage[{"content":"$nonEscape[$customEmoji[git_discussion]] *$username asks question of the day!*", "embeds":"{newEmbed:{thumbnail:$serverIcon}{description:*No off-topic in thread please, talk about what is the topic about \`❤️\`*\\n✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・\\n> **__question__: $interactionData[options.data[0].value]** \\n ✄・┈┈・┈┈・┈┈・┈┈・┈┈・┈┈・}{color:$getVar[gitHex]}}", "fetchReply": true,"options":{"interaction":true, "reactions": ["991248874238910524"]}};yes]]
 
-  $onlyForRoles[$getVar[owner];$getVar[admin];$getVar[mod];{"content":"$customEmoji[git_alert] You don't have any staff role to make question of the daily.","ephemeral": "true", "options": {"interaction": true}}]
+  $onlyForRoles[$getVar[owner];$getVar[admin];$getVar[mod];{"content":"$nonEscape[$customEmoji[git_alert]] You don't have any staff role to make question of the daily.","ephemeral": "true", "options": {"interaction": true}}]
   `
 }
