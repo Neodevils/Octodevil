4f82f38898c37d43c229d4e529b496111858c7aa
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-07-01 17:02:38 +0000

    Updated avatar.

--- commands/general/avatar.js
+++ commands/general/avatar.js
@@ -3,6 +3,6 @@ module.exports = {
   type: 'interaction',
   prototype: 'slash',
   code: `
-  $sendMessage[{"embeds": "{newEmbed:{author:$userTag[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]}{title:$customEmoji[git_mention] × Profile Picture}{description:Tap to below to go the member's user profile\\n> [Tap here]($userAvatar[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]).}{color:$getRoleColor[$highestRole[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]]}{image:$memberAvatar[$guildID;$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]}}", "fetchReply": true, "options": {"interaction": true, "reactions": ["990871412111712306", "992468316973187173", 990871472635510826"]}}]
+  $sendMessage[{"embeds": "{newEmbed:{author:$userTag[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]}{title:$customEmoji[git_mention] × Profile Picture}{description:Tap to below to go the member's user profile\\n>>>\\n [Tap here]($userAvatar[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]).}{color:$getRoleColor[$highestRole[$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]]}{image:$memberAvatar[$guildID;$replaceText[$interactionData[options.data[0].value];undefined;$authorID]]}}", "fetchReply": true, "options": {"interaction": true, "reactions": ["990871412111712306","990871472635510826"]}}]
   `
 }
