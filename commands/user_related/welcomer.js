890b8f06d72166bbeb1a63b735d1f52496eb1551
Neo <71941576+NeoAIChan@users.noreply.github.com>
2022-07-05 19:21:30 +0000

    Updated loops and welcome

--- commands/user_related/welcomer.js
+++ commands/user_related/welcomer.js
@@ -7,6 +7,8 @@ module.exports = [
 
     $giveRole[$guildID;$authorID;801607349290139688]
 
+    $useChannel[722267447935238215]
+
     $onlyForServers[697039582922801182;]
     `
   },
@@ -15,6 +17,8 @@ module.exports = [
     channel: '961144092782374942',
     code: `
     $attachment[https://api.xzusfin.repl.co/card?avatar=$replaceText[$authorAvatar;.webp;.png;1]?size=4096&middle=$uri[Goodbye;encode]&name=$replaceText[$replaceText[$username[$authorID]#$discriminator[$authorID];#;%23;-1]; ;%20;-1]&bottom=$uri[Now we are $membersCount members;encode]&background=https://media.discordapp.net/attachments/861208192121569280/993915713075032084/AddText_07-03-09.24.03.png&text=%23ffffff&avatarborder=%23fcbfcb&avatarbg=%23FF28b3;welcome.png]
+
+    $useChannel[961144092782374942]
 
     $onlyForServers[697039582922801182;]
     `
