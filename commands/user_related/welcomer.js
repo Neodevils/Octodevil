be511d9916ea5cbbedcb8fe6c4ae6c00174eeb37
neoaichan@gmail.com <neoaichan@gmail.com>
2022-07-05 22:00:28 +0300

    Updated; welcomer system.

--- commands/user_related/welcomer.js
+++ commands/user_related/welcomer.js
@@ -1,19 +1,22 @@
-module.exports = {
-	type: 'join',
-	channel: '722267447935238215',
-	code: `
-$channelSendMessage[722267447935238215;>>> <a:mns_sparkles:958967808358760458>┈ ┈ ┈ ꒦<a:mns_hearts:958968024868737084>꒷ ┈ ┈ ┈<a:mns_sparkles:958967808358760458>
-**<a:mns_welc:959328131729219594><a:mns_come:959328114272534538> <@$authorID> *!!***
-             <:mns_heart_brown:959329480084701194>   <a:mns_hearts:958968024868737084>   <:mns_heart_yellow:959323832253055017>               
-              <@&801618241046315080>
-˚꒷꒦︶︶₊꒷꒦<a:mns_hearts:958968024868737084>︶₊꒷꒦˚︶︶
-$customEmoji[kaeruMilestone] **You can start to read our <#889266702620176434> and take roles on <#754492188246802563> channel :3**]
-$wait[1s]
+module.exports = [
+  {
+    type: 'join',
+    channel: '722267447935238215',
+    code: `
+    $attachment[https://api.xzusfin.repl.co/card?avatar=$replaceText[$authorAvatar;.webp;.png;1]?size=4096&middle=$uri[Welcome to MinesaHub;encode]&name=$replaceText[$replaceText[$username[$authorID]#$discriminator[$authorID];#;%23;-1]; ;%20;-1]&bottom=$uri[Now we are $membersCount members;encode]&background=https://media.discordapp.net/attachments/861208192121569280/993915713075032084/AddText_07-03-09.24.03.png&text=%23ffffff&avatarborder=%23fcbfcb&avatarbg=%23FF28b3;welcome.png]
 
-$changeNickname[$authorID;✿ $randomText[foamy;horrible;super;puny;tender;fertile;existing;full;kindly;obeisant;troubled;gratis;lumpy;shiny;dark;needy;whimsical;soggybeam;particled;arrogant;aware;cultured;detective;economist;determined;reduction;perform;dimension;tailed;optimistic;lose;ditch;buyable;slime;shiver;curvy;nasty;small;best;cool;silly;pushy;misty;stale;one;stiff;sick;false;shut;nice;mocha;asleep;ripe;idiotic;unequaled;important;invincible;confused;ethereal;spiky;mellowroomy;uttermost;flat;calm;next;verdant;hilarious;sufficient;aspiring;excellent;toxic;devil;bright] ៸៸ $username]
+    $giveRole[$guildID;$authorID;801607349290139688]
 
-$giveRoles[$guildID;$authorID;958995945582895125;862812338674991124;862812114133844008;821910718605426688;959040410712428575;801607349290139688]
+    $onlyForServers[697039582922801182;]
+    `
+  },
+  {
+    type: 'leave',
+    channel: '961144092782374942',
+    code: `
+    $attachment[https://api.xzusfin.repl.co/card?avatar=$replaceText[$authorAvatar;.webp;.png;1]?size=4096&middle=$uri[Goodbye;encode]&name=$replaceText[$replaceText[$username[$authorID]#$discriminator[$authorID];#;%23;-1]; ;%20;-1]&bottom=$uri[Now we are $membersCount members;encode]&background=https://media.discordapp.net/attachments/861208192121569280/993915713075032084/AddText_07-03-09.24.03.png&text=%23ffffff&avatarborder=%23fcbfcb&avatarbg=%23FF28b3;welcome.png]
 
-$onlyForServers[697039582922801182;]
-`
-}
+    $onlyForServers[697039582922801182;]
+    `
+  }
+]
--- handlers/callbacks.js
+++ handlers/callbacks.js
@@ -1,12 +1,6 @@
 module.exports = (bot) => {
   bot.onMessage();
-  bot.onMessageDelete();
-  bot.onMessageUpdate();
   bot.onInteractionCreate();
   bot.onJoin();
   bot.onLeave();
-  bot.onChannelCreate();
-  bot.onChannelDelete();
-  bot.onChannelUpdate();
-  bot.onMemberUpdate();
 }
