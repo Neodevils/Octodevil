0fd90b051546c6687105f437970acee874f0cb62
Neo <neoaichan@gmail.com>
2022-08-13 19:52:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -50,22 +50,6 @@ loader.setColors({
   }
 });
 
-bot.functionManager.createCustomFunction({
-  name : '$roleIcon',
-  type : 'djs',
-  code : async (d) => {
-    const data = d.util.aoiFunc(d) // this function opens data sent from interpreter
-    const [ roleId, guildId = d.guild?.id ] = data.inside.splits //gets params
-    const guild = await d.util.getGuild(d,guildId) //gets guild object
-    if(!guild) return d.aoiError.fnError(d,"guild",{ inside: data.inside}) //sends invalid guild error
-    data.result = guild.roles.cache.get(roleId)?.iconURL({ size: 1024, dynamic : true }) // returns icon
-    return {
-      code : d.util.setCode(data), //sets code 
-    }
-  }
-});
-
-
 bot.command({
   name: "c-dev",
   code: `
